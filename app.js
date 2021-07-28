df2b7931c54e044d4960ef0db0746514ea8837c3
Estigma9 <esduro@gmail.com>
2021-07-28 09:23:59 -0500

    Crud con componentes

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Task Board with VUE COMPONENT F-304',
+        sesion: 5,
+        profesores:{
+            'nombre': 'Esteban',
+            'ciclo': '3'
+        },
+        tareas:[],
+        nuevaTarea:'', 
+        checked: false,     
+    },
+})
+
